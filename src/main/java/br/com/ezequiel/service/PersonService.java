ee49b93c9f645b3ed45f48e745e255be7807f2b9
lucasbezq <lucasbarrosez@gmail.com>
2024-05-01 19:07:05 -0300

    feat: criar implementação e classe de serviço de Person

---
+++ src/main/java/br/com/ezequiel/service/PersonService.java
@@ -0,0 +1,14 @@
+package br.com.ezequiel.service;
+
+import br.com.ezequiel.model.Person;
+
+public class PersonService implements IPersonService{
+
+    @Override
+    public Person createPerson(Person person) {
+
+        return person;
+    }
+
+}
+
