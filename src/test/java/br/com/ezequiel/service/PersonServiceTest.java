3d9b7592fbc06caff31729f25b15e498782ae437
lucasbezq <lucasbarrosez@gmail.com>
2024-05-02 11:19:20 -0300

    feat: adicionar assertions para os demais campos de Person

--- src/test/java/br/com/ezequiel/service/PersonServiceTest.java
+++ src/test/java/br/com/ezequiel/service/PersonServiceTest.java
@@ -4,6 +4,7 @@ import br.com.ezequiel.model.Person;
 import br.com.ezequiel.service.IPersonService;
 import br.com.ezequiel.service.PersonService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,13 +12,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonServiceTest {
 
+    Person person;
+
+    @BeforeEach
+    void setup() {
+        person = new Person("Lucas", "Ezequiel", "lucas@gmailcom",
+                "Ipanema", "Male");
+    }
+
     @DisplayName("When create a person with success should return a Person Object")
     @Test
     void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
         //Given
         IPersonService service = new PersonService();
-        Person person = new Person("Lucas", "Ezequiel", "lucas@gmailcom",
-                "Ipanema", "Male");
 
         //When
         Person actual = service.createPerson(person);
@@ -28,11 +35,9 @@ public class PersonServiceTest {
 
     @DisplayName("When create a person with success should contains firstName in return Person Object")
     @Test
-    void testCreatePerson_WhenSuccess_ShouldContainsFirstNameInReturnPersonObject() {
+    void testCreatePerson_WhenSuccess_ShouldContainsCorrectDataInReturnPersonObject() {
         //Given
         IPersonService service = new PersonService();
-        Person person = new Person("Lucas", "Ezequiel", "lucas@gmailcom",
-                "Ipanema", "Male");
 
         //When
         Person actual = service.createPerson(person);
@@ -40,6 +45,18 @@ public class PersonServiceTest {
         //Then
         assertEquals(person.getFirstName(), actual.getFirstName(),
                 () -> "The firstName is differet!");
+
+        assertEquals(person.getLastName(), actual.getLastName(),
+                () -> "The lastName is differet!");
+
+        assertEquals(person.getEmail(), actual.getEmail(),
+                () -> "The email is differet!");
+
+        assertEquals(person.getAddress(), actual.getAddress(),
+                () -> "The address is differet!");
+
+        assertEquals(person.getGender(), actual.getGender(),
+                () -> "The gender is differet!");
     }
 
 
